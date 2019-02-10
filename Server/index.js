b9a65034826c4790f4e921b0324be8f31638efdb
Samuel Adewole <samueladewole15@gmail.com>
2019-02-10 14:28:26 +0100

    set up node server

---
+++ Server/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+require('dotenv').config()
+
+const app = express();
+
+
+// PORT
+const port = 3000;
+app.listen(port, ()=>{
+    console.log(`You're listening to port: ${port}`)
+})
