ba1ba985bbf7066df1e65180e9138d0acc100d5a
Samuel Adewole <samueladewole15@gmail.com>
2019-02-14 00:54:04 +0100

    initialize and modified the admin-menu page

--- Front-end/js/navadmin.js
+++ Front-end/js/navadmin.js
@@ -1,3 +1,4 @@
+// navbar functions
 const navPill = document.querySelector('.nav-pills');
 const modal = document.querySelector('.modal');
 const navFlex = document.querySelector('.nav-flex');
@@ -25,3 +26,50 @@ const start = () => {
 	document.querySelector('#opennav').addEventListener('click', openNav);
 };
 start();
+
+// end
+
+// add modal function
+const modalbg = document.querySelector('.modalbg');
+
+const addMenu = () => {
+	modalbg.style.display = 'block';
+	document.querySelector('.addModal').style.marginTop = '0px';
+};
+
+const editMenu = () => {
+	const edit = document.querySelectorAll('.edit');
+	edit.forEach((i) => {
+		i.addEventListener('click', () => {
+			modalbg.style.display = 'block';
+			document.querySelector('.addModal').style.marginTop = '0px';
+		});
+	});
+};
+
+const exitModal = (e) => {
+	if (e.target == modalbg) {
+		modalbg.style.display = 'none';
+	}
+};
+
+const close = () => {
+	let close = document.querySelectorAll('.close');
+
+	close.forEach((i) => {
+		i.addEventListener('click', () => {
+			modalbg.style.display = 'none';
+		});
+	});
+};
+
+const Modal = () => {
+	window.addEventListener('click', exitModal);
+	document.querySelector('.add-menu').addEventListener('click', addMenu);
+	editMenu();
+	close();
+};
+
+Modal();
+
+// end
